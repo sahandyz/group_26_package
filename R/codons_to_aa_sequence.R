7ebcbcfd004d322b81f1d63b7f39331d9d7fac08
sahandyz <s203538@dtu.dk>
2023-10-24 11:47:02 +0200

    Updating and adding function by Sahand

---
+++ R/codons_to_aa_sequence.R
@@ -0,0 +1,12 @@
+#' Codons to Amino Acid Sequence
+#'
+#' @param codons a vector of 3-letter codons as strings
+#'
+#' @return A collapsed amino acid sequence as a string
+#' @export
+#'
+#' @examples
+codons_to_aa_sequence <- function(codons){
+  codon_to_aa <- paste0(codon_table[codons], collapse = "")
+  return(codon_to_aa)
+}
